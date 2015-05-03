2b4deaa4f432dc3d71e78c764a5efe6fcb680851
Mark Garnett <thecommunistduck@hotmail.co.uk>
2015-05-03 14:18:05 +0100

    Added descriptions.

--- game.c
+++ game.c
@@ -59,7 +59,21 @@ room rooms[NUM_ROOMS] = {
 	{"Integrated Circuit Unit, North-Central Side", "You are standing in a large, spacious room. This seems to be the main unit of the board.\n", {IC1, IC3, AVCC, LED_ROOM}},
 	{"Integrated Circuit Unit, South-Central Side", "You are standing in a large, spacious room. This seems to be the main unit of the board.\n", {IC2, IC4, USB, SCROLL_CORR}},
 	{"Integrated Circuit Unit, South Side", "You are standing in a large, spacious room. This seems to be the main unit of the board.\n", {IC3, BATT, EXT_PIN_1, CORRIDOR_3}},
+	{"Reset Button", "Above you, you can see the reset switch. You could probably just about reach it from here.\n", {NO_ROOM, NO_ROOM, NO_ROOM, IC1}},
+	{"AVCC Switch Room", "The walls pulse around you. Everything is oscillating wildly in here. It's giving you a headache.\n", {NO_ROOM, NO_ROOM, NO_ROOM, IC2}},
+	{"LED Room", "You look down. You are standing on top of the LED. It is currently off, but you can feel it humming under your feet.\n", {NO_ROOM, NO_ROOM, IC2, SD1}},
+	{"USB Port", "As you walk to the edge of the room, you look out the USB socket. It's a long, long way down. You'd probably die if you fell out.\n", {NO_ROOM, NO_ROOM, OUTSIDE_2, IC3}},
+	{"Above Scroll Wheel", "You find yourself on a silicon catwalk. Below you is a circular room, split into 5 sections. There's a rusty ladder leading down to it.\n", {NO_ROOM, NO_ROOM, IC3, NO_ROOM}},
+	{"Scroll Wheel", "You climb down the ladder and step on the floor of the circular room. The floor seems to depress when you stand on it.\n", {NO_ROOM, NO_ROOM, NO_ROOM, NO_ROOM}},
+	{"GPIO Pins", "There are a number of holes above you, light streaming in.\n", {NO_ROOM, NO_ROOM, NO_ROOM, IC4}},
+	{"A Corridor", "You find yourself in a dimly lit corridor. The walls are lined with pulsing green lights.\n", {EXT_PIN_2, ROTARY, IC4, CORRIDOR_FINAL}},
 	{"Rotary Encoder Control Room", "You are standing in a large, spacious room. This seems to be the main unit of the board.\n", {CORRIDOR_3, NO_ROOM, NO_ROOM, NO_ROOM}},
+	{"Battery Room Place", "Battery stuff.\n", {IC4, NO_ROOM, NO_ROOM, CAP_BANK}},
+	{"Capacitor Bank", "CAPACITORS ERRYWHERE.\n", {NO_ROOM, NO_ROOM, BATT, CAP_BANK}},
+	{"GPIO Pins", "There are a number of holes above you, light streaming in.\n", {NO_ROOM, CORRIDOR_3, NO_ROOM, NO_ROOM}},
+	{"An Ominous Corridor", "As you walk down the corridor, you have a very ominous feeling. Almost like something very bad is at the end of the corridor.\n", {NO_ROOM, NO_ROOM, CORRIDOR_3, NO_ROOM}},
+	{"The LCD Screen", "As you walk into the LCD screen, a cold breeze brushes over you. A giant dragon flies down from the ceiling and lands in front of you.\n", {NO_ROOM, NO_ROOM, CORRIDOR_FINAL, NO_ROOM}},
+	{"Outside, Falling", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA-splat. I told you it was a long way down. You are dead. RIP. \n", {NO_ROOM, NO_ROOM, NO_ROOM, NO_ROOM}}
 };
 
 void print_player_pos()
